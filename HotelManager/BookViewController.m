78b678de334100306af707b297fc3cf912587876
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-01 17:36:05 -0800

    Finished BookViewController

--- HotelManager/BookViewController.m
+++ HotelManager/BookViewController.m
@@ -12,10 +12,12 @@
 #import "Guest.h"
 #import "Reservations.h"
 #import "AvailabilityViewController.h"
+#import "AppDelegate.h"
 
 
 @interface BookViewController ()
 
+@property (strong, nonatomic)UITextField *namefield;
 
 
 @end
@@ -24,7 +26,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setupBookViewController];
+    [self setupMessageLabel];
+    [self setupNameField];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,14 +37,81 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void) setupBookViewController{
+    [self.navigationItem setTitle:self.room.hotel.name];
+    [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveButtonSelected:)]];
 }
-*/
+
+-(void) setupMessageLabel{
+    UILabel *messageLabel =[[UILabel alloc]init];
+    messageLabel.textAlignment = NSTextAlignmentCenter;
+    messageLabel.numberOfLines = 0;
+    messageLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+    messageLabel.text = [NSString stringWithFormat:@"Reservations at %@, Room: %i, From: %@ - To: %@", self.room.hotel.name, self.room.roomNumber.intValue, [NSDateFormatter localizedStringFromDate:self.startDate dateStyle:NSDateFormatterShortStyle timeStyle:NSDateFormatterNoStyle], [NSDateFormatter localizedStringFromDate:self.endDate dateStyle:NSDateFormatterShortStyle timeStyle:NSDateFormatterNoStyle]];
+    
+    [self.view addSubview:messageLabel];
+    
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:messageLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0 constant:20.0];
+    
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:messageLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:-20.0];
+    
+    NSLayoutConstraint *centerY = [NSLayoutConstraint constraintWithItem:messageLabel attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0];
+    
+    leading.active = YES;
+    trailing.active = YES;
+    centerY.active = YES;
+}
+
+-(void) setupNameField{
+    self.namefield = [[UITextField alloc]init];
+    self.namefield.placeholder = @"Please enter your name";
+    self.namefield.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview:self.namefield];
+    
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:self.namefield attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0 constant:20.0];
+    
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.namefield attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:84.0];
+    
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:self.namefield attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:-20.0];
+    
+    leading.active = YES;
+    top.active = YES;
+    trailing.active = YES;
+    
+    [self.namefield becomeFirstResponder];
+    
+}
+-(void)saveButtonSelected:(UIBarButtonItem *)sender{
+    
+    AppDelegate *delegate = (AppDelegate *) [[UIApplication sharedApplication]delegate];
+    
+    NSManagedObjectContext *context = delegate.managedObjectContext;
+    
+    Reservations *reservation = [NSEntityDescription insertNewObjectForEntityForName:@"Reservations" inManagedObjectContext:context
+                                 ];
+    reservation.startDate = self.startDate;
+    reservation.endDate = self.endDate;
+    reservation.room = self.room;
+    
+    self.room.reservation = reservation;
+    
+    Guest *guest = [NSEntityDescription insertNewObjectForEntityForName:@"Guest" inManagedObjectContext:context];
+    
+    guest.name = self.namefield.text;
+    reservation.guest = guest;
+   
+    NSError *sendError;
+    [context save:&sendError];
+    if (sendError) {
+        NSLog(@"Error when saving");
+        
+    }else {
+        
+        [self.navigationController popToRootViewControllerAnimated:YES];
+    }
+    
+}
 
 @end
